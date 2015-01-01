9bb4cbf3ff3ef4fd9b8b6dbfb1cf8e7f85563289
Akash Pal <akashpa93@gmail.com>
2015-01-01 17:56:15 +0530

    Add context for each location marker on map when clicked.

--- js/helper.js
+++ js/helper.js
@@ -161,16 +161,17 @@ function initializeMap() {
       title: name
     });
 
+     var contentString = '<p style="color: #1199C3;"><b>'+ name +'</b></p>';
     // infoWindows are the little helper windows that open when you click
     // or hover over a pin on a map. They usually contain more information
     // about a location.
     var infoWindow = new google.maps.InfoWindow({
-      content: name
+      content: contentString
     });
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+       infoWindow.open(map,marker);
     });
 
     // this is where the pin actually gets added to the map.
